a4f8dcd5b7d29121ef7d8de9ff52d95be15fd6af
gupta-kush <kushg@tamu.edu>
2025-04-18 19:51:04 -0500

    Refined title styling and splash positioning

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const splashTexts = [
-    //"Now in JavaScript!",
-    //"Hotter than the sun!",
     "Hello World!",
-    //"CTRL + S to save!",
+    //"Creeper? Aww man.",
     //"Built by Kush!",
-    "Creeper? Aww man.",
     //"A CS student approaches!"
-  ];
-  
-  const splash = document.getElementById("splash-text");
-  const pick = splashTexts[Math.floor(Math.random() * splashTexts.length)];
-  splash.textContent = pick;
-  
+];
+
+const splash = document.getElementById("splash-text");
+const pick = splashTexts[Math.floor(Math.random() * splashTexts.length)];
+splash.textContent = pick;
