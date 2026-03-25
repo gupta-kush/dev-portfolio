151363580f5f250efa233635a0f2413930a42851
Kush Gupta <54411218+gupta-kush@users.noreply.github.com>
2026-03-25 10:05:09 -0500

    feat: implement global smooth scrolling and custom mouse cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
+import { useEffect } from "react";
+import Lenis from "lenis";
 import { Hero } from "./components/Hero";
 import { Projects } from "./components/Projects";
 import { Photography } from "./components/Photography";
 import { Footer } from "./components/Footer";
 import { Navbar } from "./components/Navbar";
 import { ThemeToggle } from "./components/ThemeToggle";
+import { CustomCursor } from "./components/CustomCursor";
 
 export default function App() {
+  useEffect(() => {
+    const lenis = new Lenis();
+    
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    
+    requestAnimationFrame(raf);
+    
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <div id="top" className="min-h-screen flex flex-col">
+      <CustomCursor />
       <Navbar />
       <ThemeToggle />
       <main className="flex-grow">
